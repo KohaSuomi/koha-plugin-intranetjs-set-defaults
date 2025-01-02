c476dd2e6387feeb6b8af71ee91ac2a3ba75cfb6
Lari Strand <lmstrand@gmail.com>
2025-01-02 15:33:54 +0200

    Add plugin type to plugin_data on install

--- Koha/Plugin/Fi/KohaSuomi/SetDefaults.pm
+++ Koha/Plugin/Fi/KohaSuomi/SetDefaults.pm
@@ -135,6 +135,12 @@ sub configure {
 sub install() {
     my ( $self, $args ) = @_;
 
+    $self->store_data(
+            {
+                type => 'intranetUserJs',
+            }
+        );
+        
     return 1;
 }
 
