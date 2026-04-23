762fe989dc34214ec5168cb5e385f65a8925e1c1
lmstrand <lmstrand@gmail.com>
2026-04-23 12:37:35 +0300

    KOHA-2228 add Force primary email as default for patron main contact method

--- Koha/Plugin/Fi/KohaSuomi/SetDefaults.pm
+++ Koha/Plugin/Fi/KohaSuomi/SetDefaults.pm
@@ -36,7 +36,7 @@ sub new {
 
     ## Here, we call the 'new' method for our base class
     ## This runs some additional magic and checking
-    ## and returns our actual 
+    ## and returns our actual
     my $self = $class->SUPER::new($args);
 
     return $self;
@@ -52,20 +52,23 @@ sub intranet_js {
     my $dir=C4::Context->config('pluginsdir');
     my $plugin_fulldir = $dir . "/Koha/Plugin/Fi/KohaSuomi/SetDefaults/";
     my $js = read_file($plugin_fulldir .'script.js');
-    
+
     my $param_a = $self->retrieve_data('config_param_a') || 0;
     my $param_b = $self->retrieve_data('config_param_b') || 0;
     my $param_c = $self->retrieve_data('config_param_c') || 0;
     my $param_d = $self->retrieve_data('config_param_d') || 0;
     my $param_e = $self->retrieve_data('config_param_e') || 0;
-    
+    my $param_f = $self->retrieve_data('config_param_f') || 0;
+
     # Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
     $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
     $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_B/$param_b/r;
     $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_C/$param_c/r;
     $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_D/$param_d/r;
     $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_E/$param_e/r;
-    
+    $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_F/$param_f/r;
+
+
     utf8::decode($js);
     return "<script>$js</script>";
 }
@@ -76,18 +79,18 @@ sub intranet_js {
 ## Koha database should be considered a tool
 sub admin {
     my ( $self, $args ) = @_;
-    
+
     my $cgi = $self->{'cgi'};
     my $template = $self->get_template({ file => 'viewjs.tt' });
-    
+
     my $plugin_fulldir = $self->mbf_path();
     my $js = read_file($plugin_fulldir .'script.js');
-    
+
     # my $param_a = $self->retrieve_data('config_param_a');
-    
+
     ## Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
     # $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
-    
+
     utf8::decode($js);
     $template->param( 'jscontent' => $js );
 
@@ -112,6 +115,7 @@ sub configure {
             config_param_c => $self->retrieve_data('config_param_c'),
             config_param_d => $self->retrieve_data('config_param_d'),
             config_param_e => $self->retrieve_data('config_param_e'),
+            config_param_f => $self->retrieve_data('config_param_f'),
             last_upgraded   => $self->retrieve_data('last_upgraded'),
         );
 
@@ -125,6 +129,7 @@ sub configure {
                 config_param_c => $cgi->param('config_param_c'),
                 config_param_d => $cgi->param('config_param_d'),
                 config_param_e => $cgi->param('config_param_e'),
+                config_param_f => $cgi->param('config_param_f'),
                 last_configured_by => C4::Context->userenv->{'number'},
             }
         );
@@ -144,7 +149,7 @@ sub install() {
                 type => 'intranetUserJs',
             }
         );
-        
+
     return 1;
 }
 
--- Koha/Plugin/Fi/KohaSuomi/SetDefaults/script.js
+++ Koha/Plugin/Fi/KohaSuomi/SetDefaults/script.js
@@ -3,6 +3,7 @@ var param_b = REPLACE_BY_CONFIG_PARAM_B;
 var param_c = REPLACE_BY_CONFIG_PARAM_C;
 var param_d = REPLACE_BY_CONFIG_PARAM_D;
 var param_e = REPLACE_BY_CONFIG_PARAM_E;
+var param_f = REPLACE_BY_CONFIG_PARAM_F;
 
 /// ALKU ///
 /* Laita raksi "Poista käsin poistettujen varausten maksut" -kohtaa palautuksessa. Tällä estetään noutamattoman varauksen maksun syntyminen, kun varaus noudettavissa oleva varaus poistetaan palautuksen kautta. Huom! Ei toimi, jos palautus tehdään muualla kuin Palautus-sivulla. */
@@ -65,6 +66,18 @@ $(document).ready(function () {
         if (window.location.href.indexOf("members/memberentry.pl") > -1) {
             $('.relationship option:eq(1)').attr('selected', 'selected');
             $(".new_guarantor_relationship option:eq(1)").attr('selected', 'selected');
+        }
+    }
+});
+///LOPPU///
+
+///ALKU///
+//Asiakastakaaja-osion Suhde-arvo valmiiksi valituksi
+$(document).ready(function () {
+    if (param_f == "1"){
+        if (window.location.href.indexOf("members/memberentry.pl") > -1) {
+           $('#primary_contact_method option[value="email"]').attr('selected', 'selected');
+           $('#primary_contact_method').change();
         }
     }
 });
